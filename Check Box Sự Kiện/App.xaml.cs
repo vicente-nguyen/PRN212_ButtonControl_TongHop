13227b53ccff2d59b89eb1042d40b6949694f014
Hiep Nguyen <hipnguyendut25@gmail.com>
2025-06-17 10:38:07 +0700

    Control coban 2

---
+++ Check Box Sự Kiện/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Check_Box_Sự_Kiện
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
---
+++ Check Box Sự Kiện/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Check_Box_Sự_Kiện
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void ChkToanBo_Checked(object sender, RoutedEventArgs e)
+        {
+            chkCongNghe.IsChecked = true;
+            chkDuLich.IsChecked = true;
+            chkDaBong.IsChecked = true;
+            chkXemPhim.IsChecked = true;
+        }
+        private void ChkToanBo_Unchecked(object sender, RoutedEventArgs e)
+        {
+            chkCongNghe.IsChecked = false;
+            chkDuLich.IsChecked = false;
+            chkDaBong.IsChecked = false;
+            chkXemPhim.IsChecked = false;
+        }
+        private void chkSub_CheckedChanged(object sender, RoutedEventArgs e)
+        {
+            if (chkCongNghe.IsChecked == true &&
+                chkDuLich.IsChecked == true &&
+                chkDaBong.IsChecked == true &&
+                chkXemPhim.IsChecked == true)
+            {
+                chkToanBo.IsChecked = true;
+            }
+            else if (chkCongNghe.IsChecked == false &&
+                     chkDuLich.IsChecked == false &&
+                     chkDaBong.IsChecked == false &&
+                     chkXemPhim.IsChecked == false)
+            {
+                chkToanBo.IsChecked = false;
+            }
+        }
+    }
+}       
