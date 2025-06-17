c3bba2d401eaffff89186f17c83ad2f24bba3c76
Hiep Nguyen <hipnguyendut25@gmail.com>
2025-06-17 10:55:22 +0700

    control bai tap 3

--- RadioButton Control/MainWindow.xaml.cs
+++ RadioButton Control/MainWindow.xaml.cs
@@ -20,5 +20,39 @@ namespace RadioButton_Control
         {
             InitializeComponent();
         }
+        private void BtnGui_Click(object sender, RoutedEventArgs e)
+        {
+            string binhchon = "";
+            if (radRatTot.IsChecked == true)
+                binhchon = radRatTot.Content + "";
+            else if (radTot.IsChecked == true)
+                binhchon = radTot.Content + "";
+            else if (radTamDuoc.IsChecked == true)
+                binhchon = radTamDuoc.Content + "";
+            else if (radKhongTot.IsChecked == true)
+                binhchon = radKhongTot.Content + "";
+            string gioitinh = "";
+            if (radNam.IsChecked == true)
+                gioitinh = radNam.Content + "";
+            else if (radNu.IsChecked == true)
+                gioitinh = radNu.Content + "";
+            string infor = "Bạn bình chọn hệ thống=" + binhchon + Environment.NewLine;
+            infor += "Giới tính của bạn=" + gioitinh;
+            MessageBoxResult ret;
+            ret = MessageBox.Show(infor, "Mời bạn xác nhận", MessageBoxButton.YesNo, MessageBoxImage.Question);
+            if (ret == MessageBoxResult.Yes)
+            {
+                //ghi nhan
+            }
+        }
+        private void BtnHuy_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+            //hoac close();
+        }
+        
+                
+        
     }
-}
+
+    }
